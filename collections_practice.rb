ff8f57065dce379af73b4f2dd9435531bc54f273
excellent-revision-4527 <tjcroley@outlook.com>
2020-02-22 03:14:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,7 +12,7 @@ def sort_array_char_count(strings)
   strings.sort {|left, right| left.length <=> right.length}
 end
 
-def swap_elements_from_to(array, index, destination_index)
+def swap_elements(array, index, destination_index)
   array[index], array[destination_index] = array[destination_index], array[index]
   return array
 end
