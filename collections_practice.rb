79df986011934a999f51e33f29662d9413b782e5
excellent-revision-4527 <tjcroley@outlook.com>
2020-02-22 03:12:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,5 +9,5 @@ def sort_array_desc(integers)
 end
 
 def sort_array_char_count(strings)
-  strings.sort {|left, right| left.length <=> right.length
+  strings.sort {|left, right| left.length <=> right.length}
 end
